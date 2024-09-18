1ddb0eedf9d1924266018ee26e6ed1d8bca40b1a
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-09-18 12:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-// complete this js code
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  
+  greet() {  // Greet method for Person class
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+
+class Employee extends Person { // Employee class extending Person
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call the parent class constructor with name and age
+    this.jobTitle = jobTitle; // Additional property specific to Employee
+  }
+
+  
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);// jobGreet method for Employee class
+  }
+}
 
 // Do not change code below this line
 window.Person = Person;
